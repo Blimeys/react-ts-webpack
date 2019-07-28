541fe1aeb0bf7eb522f970a32eb5d4b78decb44b
Jean Dulck <djeanmath@gmail.com>
2019-07-28 13:53:23 +0100

    Basic config

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Welcome = () => <h1>No create-bullcrap</h1>;
+
+ReactDOM.render(<Welcome />, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: { contentBase: './dist' },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: './index.html'
+    })
+  ]
+};
